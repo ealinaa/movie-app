bb450b1a7bdb3b2a9bb87cbba463da47ac237904
ealinaa <alinaadhikari108@gmail.com>
2026-06-29 20:58:01 +0545

    adding routes

--- QuickShow/frontend/src/App.jsx
+++ QuickShow/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
 import MovieDetail from './pages/MovieDetail'
@@ -8,12 +8,20 @@ import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
 import Navbar from './components/Navbar'
+import { Toaster } from "react-hot-toast"
+import Footer from './components/Footer'
 
 
 const App = () => {
+
+  //to hide navbar in admin page
+   const isAdminRoute = useLocation().pathname.startsWith('/admin')
+ 
   return (
     <>
-    <Navbar/>
+    {/* to enable the feature of notification in all the componenet we used toaster */}
+    <Toaster />
+    {!isAdminRoute && <Navbar/>}
     
     <Routes>
       <Route path='/' element={<Home/>}/>
@@ -25,6 +33,8 @@ const App = () => {
 
 
     </Routes>
+    {/* this footer in shown in all the pages expect admin */}
+     {!isAdminRoute && <Footer/>}
     </>
   )
 }
--- QuickShow/frontend/src/pages/Home.jsx
+++ QuickShow/frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <h1 class ='text-2xl'>home</h1>
   )
 }
 
