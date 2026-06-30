97571f375906b8d8357b429a2e96faa24f62632e
ealinaa <alinaadhikari108@gmail.com>
2026-06-30 22:57:32 +0545

    working with navbar page

--- QuickShow/frontend/src/assets/assets.js
+++ QuickShow/frontend/src/assets/assets.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg'
-import marvelLogo from './marvelLogo.svg'
-import googlePlay from './googlePlay.svg'
-import appStore from './appStore.svg'
-import screenImage from './screenImage.svg'
-import profile from './profile.png'
+// import marvelLogo from './marvelLogo.svg'
+// import googlePlay from './googlePlay.svg'
+// import appStore from './appStore.svg'
+// import screenImage from './screenImage.svg'
+// import profile from './profile.png'
 
 export const assets = {
     logo,
-    marvelLogo,
-    googlePlay,
-    appStore,
-    screenImage,
-    profile
+    // marvelLogo,
+    // googlePlay,
+    // appStore,
+    // screenImage,
+    // profile
 }
 
 export const dummyTrailers = [
