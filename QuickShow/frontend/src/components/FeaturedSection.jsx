b416b9142abc22b2b836169f50efbe49b068356b
ealinaa <alinaadhikari108@gmail.com>
2026-07-07 08:06:58 +0545

    working with component herosection and featured section

--- QuickShow/frontend/src/components/FeaturedSection.jsx
+++ QuickShow/frontend/src/components/FeaturedSection.jsx
@@ -1,6 +1,9 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import BlurSection from './BlurSection'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from './MovieCard.jsx'
 
 const FeaturedSection = () => {
     const navigate = useNavigate()
@@ -8,17 +11,26 @@ const FeaturedSection = () => {
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
         {/* title */}
         <div className='relative flex items-center justify-between pt-20 pb-10'>
+            <BlurSection top='0' right='-80px'/>
             <p className='text-gray-300 font-medium text-lg'>Now showing</p>
-            <button onClick={()=>navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300'>
+            <button onClick={()=>navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300 cursor-pointer'>
                 View All 
                 <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5'/> 
             </button>
         </div>
         {/* movies list */}
+       
+        <div className='flex flex-wrap max-sm:justify-center gap-1 mt-8'>
+             {dummyShowsData.slice(0,4).map((show)=>(
+            <MovieCard key={show.id} movie={show}/>
+        ))}
 
-        <div></div>
+        </div>
         {/* button */}
-        <div></div>
+        <div className='flex justify-center mt-20'>
+            <button onClick={()=> {navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull
+            transition rounded-md font-medium cursor-pointer'>Show More</button>
+        </div>
     </div>
   )
 }
