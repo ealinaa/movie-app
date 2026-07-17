302c460fbf3d750e1fb9fe71fe3c5b82d51d1d75
ealinaa <alinaadhikari108@gmail.com>
2026-07-17 20:43:39 +0545

    working with movies

--- QuickShow/frontend/src/assets/assets.js
+++ QuickShow/frontend/src/assets/assets.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg'
 import marvelLogo from './marvelLogo.svg'
-// import googlePlay from './googlePlay.svg'
-// import appStore from './appStore.svg'
+import googlePlay from './googlePlay.svg'
+import appStore from './appStore.svg'
 // import screenImage from './screenImage.svg'
 // import profile from './profile.png'
 
 export const assets = {
     logo,
     marvelLogo,
-    // googlePlay,
-    // appStore,
+    googlePlay,
+    appStore,
     // screenImage,
     // profile
 }
