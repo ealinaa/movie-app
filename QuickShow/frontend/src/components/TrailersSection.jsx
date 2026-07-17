ec6ecd71789825f0ea3b8313b6b32449358ec073
ealinaa <alinaadhikari108@gmail.com>
2026-07-17 14:16:09 +0545

    working with component

--- QuickShow/frontend/src/components/TrailersSection.jsx
+++ QuickShow/frontend/src/components/TrailersSection.jsx
@@ -36,6 +36,7 @@ import React, { useState } from "react";
 import { dummyTrailers } from "../assets/assets";
 import ReactPlayerModule from "react-player";
 import BlurSection from "./BlurSection";
+import { PlayCircleIcon } from "lucide-react";
 
 // Get the correct component
 const ReactPlayer = ReactPlayerModule.default || ReactPlayerModule;
@@ -44,7 +45,7 @@ console.log("ReactPlayerModule:", ReactPlayerModule);
 console.log("ReactPlayer:", ReactPlayer);
 
 const TrailersSection = () => {
-  const [currentTrailer] = useState(dummyTrailers[0]);
+  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
 
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden">
@@ -59,6 +60,19 @@ const TrailersSection = () => {
           controls= {false}
           className="mx-auto max-w-full " width="960px" height ="540px"/>
         
+      </div>
+      <div className="group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto ">
+    {dummyTrailers.map((trailer) =>(
+        <div key={trailer.image}
+        className="relative group-hover:not-hover:opacity-50 hover:translate-y-1 duration-300
+        transition max-md:h-60 md:max-h-60 cursor-pointer" onClick={()=>setCurrentTrailer(trailer)}>
+            <img src={trailer.image} alt="trailer" className="rounded-lg w-full h-full object-cover brightness-75"/>
+            <PlayCircleIcon strokeWidth={1.6} className="absolute top-1/2 left-1/2
+            md:w-8 w-5 h-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2"
+             />
+            </div>
+    ))}
+
       </div>
     </div>
   );
