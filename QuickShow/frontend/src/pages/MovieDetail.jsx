51740423a475049efc74a7a6aa74c43c00ac03fe
ealinaa <alinaadhikari108@gmail.com>
2026-07-21 14:21:40 +0545

    working with movieDetail

--- QuickShow/frontend/src/pages/MovieDetail.jsx
+++ QuickShow/frontend/src/pages/MovieDetail.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurSection from '../components/BlurSection'
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
 import timeFormat from '../lib/timeFormat'
 import DataSelect from '../components/DataSelect'
+import MovieCard from '../components/MovieCard'
 
 const MovieDetail = () => {
+  const navigate = useNavigate()
   const {id} = useParams()
   const [show, setShow] = useState(null)
 
   const getShow = async () => {
     const show = dummyShowsData.find(show => show._id === id)
-    setShow({
+    if(show) {
+      setShow({
       movie: show,
       dateTime: dummyDateTimeData
     })
+
+    }
+    
   }
   useEffect(()=>{
     getShow()
@@ -67,6 +73,18 @@ const MovieDetail = () => {
 
       </div>
       <DataSelect dateTime={show.dateTime} id={id} />
+
+      <p className='text-lg font-medium mt-20 mb-8'>You May Also Like</p>
+      <div className='flex flex-wrap max-sm:justify-center gap-8'>
+        {dummyShowsData.slice(0,6).map((movie,index)=> (
+          <MovieCard key={index} movie={movie} />
+        ))}
+
+      </div>
+      <div className='flex justify-center mt-20'>
+        <button onClick={() => {navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull
+        transition rounded-md font-medium cursor-pointer'>show More</button>
+      </div>
 
     </div>
   ) : <div>Loading....</div>
