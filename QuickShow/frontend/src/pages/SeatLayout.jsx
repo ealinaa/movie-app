99d96905da36bbf3a6c99b62b96aa22d38133275
ealinaa <alinaadhikari108@gmail.com>
2026-07-25 22:09:22 +0545

    working with seatlayout

--- QuickShow/frontend/src/pages/SeatLayout.jsx
+++ QuickShow/frontend/src/pages/SeatLayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { assets, dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
-import { ClockIcon } from 'lucide-react'
+import { ArrowRightIcon, ClockIcon } from 'lucide-react'
 import isoTimeFormat from '../lib/isoTimeFormat'
 import BlurSection from '../components/BlurSection'
 import toast from 'react-hot-toast'
@@ -42,7 +42,7 @@ const renderSeats = (row, count = 12) => (
       {Array.from({ length: count}, (_, i)=>{
         const seatId = `${row}${i + 1}`;
         return (
-          <button key={seatId} onClick={() => handleSeatClick
+          <button key={seatId} onClick={() => handleSeats
             (seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer
               ${selectedSeats.includes(seatId) && 
               "bg-primary text-white"}`}>
@@ -98,10 +98,7 @@ useEffect(() => {
           <div className='grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6 '>
             {groupRows[0].map(row => renderSeats(row))}
           </div>
-
-        </div>
-
-        <div className='grid grid-cols-2 gap-11'>
+          <div className='grid grid-cols-2 gap-11'>
           {groupRows.slice(1).map((group, index) => (
             <div key={index}>
               {group.map(row => renderSeats(row))}
@@ -111,6 +108,17 @@ useEffect(() => {
           ))}
 
         </div>
+
+        </div>
+
+        <button onClick={()=> navigate('/my-bookings')} className='flex items-center gap-1
+        mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition
+        rounded-full font-medium cursor-pointer active:scale-95'>
+          Proceed to Checkout
+          <ArrowRightIcon  strokeWidth={3} className='w-4 h-4'/>
+        </button>
+
+        
 
 
 
