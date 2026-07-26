06222579caeedb5ee0cd89139c6ff8a6c4fd52e6
ealinaa <alinaadhikari108@gmail.com>
2026-07-26 20:50:06 +0545

    working with myBookings

--- QuickShow/frontend/src/pages/MyBookings.jsx
+++ QuickShow/frontend/src/pages/MyBookings.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../assets/assets'
+import BlurSection from '../components/BlurSection'
 
 const MyBookings = () => {
-  return (
-    <div>MyBookings</div>
-  )
-}
+  const currency = import.meta.env.VITE_CURRENCY
 
+  const [bookings, setBookings]= useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getMyBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getMyBookings()
+  },[])
+  return  !isLoading ? (
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh'>
+      <BlurSection top='100px' left='100px'/>
+
+    </div>
+  ): <Loading />
+}
 export default MyBookings
