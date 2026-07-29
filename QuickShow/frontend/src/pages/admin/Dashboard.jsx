50186c8cc63cadc757590a676e60e888c8a20764
ealinaa <alinaadhikari108@gmail.com>
2026-07-29 20:49:20 +0545

    working with admin dashboardr

--- QuickShow/frontend/src/pages/admin/Dashboard.jsx
+++ QuickShow/frontend/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { ChartLineIcon, CircleDollarSignIcon, icons, PlayCircleIcon, UserIcon } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/Loading';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [dashboardData, setDashboardData] = useState({
+        totalBookings:0,
+        totalRevenue: 0,
+        activeShows: [],
+        totalUser: 0
+    });
+
+    const [loading, setLoading] = useState(true);
+
+    const dashboardCards = [
+        { title: "Total Bookings", value: dashboardData.totalBookings || "0", icon: ChartLineIcon},
+         { title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon},
+          { title: "Total Shows", value: dashboardData.activeShows.length || "0", icon: PlayCircleIcon},
+           { title: "Total Users", value: dashboardData.totalUser || "0", icon: UserIcon},
+    ]
+
+    const fetchDashboardData = async () => {
+        setDashboardData(dummyDashboardData)
+        setLoading(false)
+    };
+    useEffect(() => {
+        fetchDashboardData();
+    }, [])
+  return !loading ? (
+    <div>
+        
+    </div>
+  ) : <Loading />
 }
 
 export default Dashboard
