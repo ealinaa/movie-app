eaf72efac40f80082ca732010632bc8552fcf219
ealinaa <alinaadhikari108@gmail.com>
2026-07-30 22:06:34 +0545

    working with admin dashboard

--- QuickShow/frontend/src/pages/admin/Dashboard.jsx
+++ QuickShow/frontend/src/pages/admin/Dashboard.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
+import BlurSection from '../../components/BlurSection';
 
 
 
@@ -36,8 +37,23 @@ const Dashboard = () => {
     <>
     <Title text1="Admin" text2="Dashboard" />
 
-    <div>
-        
+    <div className='relative flex flex-wrap gap-4 mt-6'>
+        <BlurSection top='-100px' left='0' />
+        <div className='flex flex-wrap gap-4 w-full'>
+            {dashboardCards.map((card, index) => (
+                <div key={index} className='flex items-center
+                justify-between px-4 py-3 bg-primary/10 border
+                border-primary/20 rounded-md max-w-50 w-full'>
+                    <div>
+                        <h1 className='text-sm'>{card.title}</h1>
+                        <p className='text-xl font-medium mt-1'>{card.value}</p>
+                    </div>
+                <card.icon className='w-6 h-6' />
+                    </div>
+
+            ))}
+
+        </div>
     </div>
 
     </>
