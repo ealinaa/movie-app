f1ee5477181f3f22d3780f956f0b286366bf618d
ealinaa <alinaadhikari108@gmail.com>
2026-08-01 20:27:12 +0545

    working with admin dashboard

--- QuickShow/frontend/src/pages/admin/Dashboard.jsx
+++ QuickShow/frontend/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,10 @@
-import { ChartLineIcon, CircleDollarSignIcon, icons, PlayCircleIcon, UserIcon } from 'lucide-react';
+import { ChartLineIcon, CircleDollarSignIcon, icons, PlayCircleIcon, StarIcon, UserIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
 import BlurSection from '../../components/BlurSection';
+import { dateFormat } from '../../lib/dateFormat';
 
 
 
@@ -56,6 +57,30 @@ const Dashboard = () => {
         </div>
     </div>
     <p className='mt-10 text-lg font-medium'>Active Shows</p>
+    <div className='relative flex flex-wrap gap-6 mt-4 max-w-5xl'>
+        <BlurSection top='100px' left='-10%' />
+        {dashboardData.activeShows.map((show) => (
+            <div key={show._id} className='w-55 rounded-lg overflow-hidden
+            h-full pb-3 bg-primary/10 border border-primary/20
+            hover:translate-y-1 transition duration-300'>
+                <img src={show.movie.poster_path} alt="" className='h-60
+                w-full object-cover' />
+                <p className='font-medium p-2 truncate'>{show.movie.title}</p>
+                <div className='flex items-center justify-between px-2'>
+                    <p className='text-lg font-medium'>{currency} {show.showPrice}</p>
+                    <p className='flex items-center gap-1 text-sm text-gray-300 mt-1 pr-1'>
+                        <StarIcon className='w-4 h-4 text-primary fill-primary' />
+                        {show.movie.vote_average.toFixed(1)}
+
+                    </p>
+
+                </div>
+                <p className='px-2 pt-2 text-sm text-gray-500'>{dateFormat(show.showDateTime)}</p>
+
+                </div>
+        ))}
+
+    </div>
 
     </>
   ) : <Loading />
