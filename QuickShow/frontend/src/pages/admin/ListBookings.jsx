af01a4ef17c405f3f4edc5273010bbe575c6c0ec
ealinaa <alinaadhikari108@gmail.com>
2026-08-04 20:33:08 +0545

    working with admin listshows

--- QuickShow/frontend/src/pages/admin/ListBookings.jsx
+++ QuickShow/frontend/src/pages/admin/ListBookings.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
 
 const ListBookings = () => {
     const currency = import.meta.env.VITE_CURRENCY
 
     const [bookings, setBookings] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    const getAllBookings = async () => {
+        setBookings(dummyBookingData)
+        setIsLoading(false);
+    }
   return (
     <div>ListBookings</div>
   )
