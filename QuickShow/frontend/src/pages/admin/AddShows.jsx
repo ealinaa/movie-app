15bfe83ce3454812a0e2f4e04183e2e67218ddd5
ealinaa <alinaadhikari108@gmail.com>
2026-08-05 15:59:59 +0545

    working with admin addshows

--- QuickShow/frontend/src/pages/admin/AddShows.jsx
+++ QuickShow/frontend/src/pages/admin/AddShows.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
 
 const AddShows = () => {
-  return (
-    <div>AddShows</div>
-  )
+    const currency = import.meta.env.VITE_CURRENCY
+    const [nowPlayingMovies, setNowPlayingMovies] =useState([]);
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    const [dateTimeSelection, setDateTimeSelection] = useState({});
+    const [dateTimeInput, setDateTimeInput] = useState("");
+    const [showPrice, setShowPrice] = useState("");
+
+    const fetchNowPlayingMovies = async () => {
+        setNowPlayingMovies(dummyShowsData)
+    };
+    useEffect(() => {
+        fetchNowPlayingMovies()
+    }, [])
+  return nowPlayingMovies.length > 0 ? (
+    <>
+    <Title text1= "Add" text2 = "Shows" />
+    <p className='mt-10 text-lg font-medium'>Now Playing Movies</p>
+
+    </>
+  ) : <Loading />
 }
 
 export default AddShows
